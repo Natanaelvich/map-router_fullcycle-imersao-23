f436421eceba1884ec3ab2c4c409c273fd2399f2
natanaelvich <taelima1997@gmail.com>
2023-08-20 11:45:47 -0300

    update imports modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { RoutesModule } from './routes/routes.module';
+import { MapsModule } from './maps/maps.module';
 
 @Module({
-  imports: [PrismaModule, RoutesModule],
+  imports: [PrismaModule, RoutesModule, MapsModule],
   controllers: [AppController],
   providers: [AppService],
 })
