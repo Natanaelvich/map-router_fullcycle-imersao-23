511c96c84df5151a29f7e31bc8d87207bff624f6
natanaelvich <taelima1997@gmail.com>
2023-08-20 11:40:49 -0300

    fix lint

--- backend/src/routes/routes.controller.ts
+++ backend/src/routes/routes.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { UpdateRouteDto } from './dto/update-route.dto';
