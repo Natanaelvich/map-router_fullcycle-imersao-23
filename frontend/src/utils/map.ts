03b9dbe4bb8a1752dfc82a56646aa4fa1b918dde
natanaelvich <taelima1997@gmail.com>
2023-08-21 22:24:46 -0300

    fix lint errors

--- frontend/src/utils/map.ts
+++ frontend/src/utils/map.ts
@@ -1,5 +1,67 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { sample, shuffle } from 'lodash'
 import type { DirectionsResponseData } from '@googlemaps/google-maps-services-js'
+
+export class Route {
+  public startMarker: google.maps.Marker
+  public endMarker: google.maps.Marker
+  public carMarker: google.maps.Marker
+
+  public directionsRenderer: google.maps.DirectionsRenderer
+
+  constructor(options: {
+    startMarkerOptions: google.maps.MarkerOptions
+    endMarkerOptions: google.maps.MarkerOptions
+    carMarkerOptions: google.maps.MarkerOptions
+  }) {
+    const { startMarkerOptions, endMarkerOptions, carMarkerOptions } = options
+    this.startMarker = new google.maps.Marker(startMarkerOptions)
+    this.endMarker = new google.maps.Marker(endMarkerOptions)
+    this.carMarker = new google.maps.Marker(carMarkerOptions)
+
+    const { strokeColor } = this.startMarker.getIcon() as google.maps.Symbol
+    this.directionsRenderer = new google.maps.DirectionsRenderer({
+      suppressMarkers: true,
+      polylineOptions: {
+        strokeColor,
+        strokeOpacity: 0.5,
+        strokeWeight: 5,
+      },
+    })
+    this.directionsRenderer.setMap(this.startMarker.getMap() as google.maps.Map)
+  }
+
+  async calculateRoute(
+    directionsResponseData?: DirectionsResponseData & { request: any },
+  ) {
+    if (directionsResponseData) {
+      const directionsResult = convertDirectionsResponseToDirectionsResult(
+        directionsResponseData,
+      )
+      this.directionsRenderer.setDirections(directionsResult)
+      return
+    }
+
+    const startPosition = this.startMarker.getPosition() as google.maps.LatLng
+    const endPosition = this.endMarker.getPosition() as google.maps.LatLng
+
+    const result = await new google.maps.DirectionsService().route({
+      origin: startPosition,
+      destination: endPosition,
+      travelMode: google.maps.TravelMode.DRIVING,
+    })
+    this.directionsRenderer.setDirections(result)
+  }
+
+  delete() {
+    this.startMarker.setMap(null)
+    this.endMarker.setMap(null)
+    this.carMarker.setMap(null)
+    this.directionsRenderer.setMap(null)
+  }
+}
 
 export class Map {
   public map: google.maps.Map
@@ -182,65 +244,6 @@ export class Map {
 }
 
 export class RouteExistsError extends Error {}
-
-export class Route {
-  public startMarker: google.maps.Marker
-  public endMarker: google.maps.Marker
-  public carMarker: google.maps.Marker
-
-  public directionsRenderer: google.maps.DirectionsRenderer
-
-  constructor(options: {
-    startMarkerOptions: google.maps.MarkerOptions
-    endMarkerOptions: google.maps.MarkerOptions
-    carMarkerOptions: google.maps.MarkerOptions
-  }) {
-    const { startMarkerOptions, endMarkerOptions, carMarkerOptions } = options
-    this.startMarker = new google.maps.Marker(startMarkerOptions)
-    this.endMarker = new google.maps.Marker(endMarkerOptions)
-    this.carMarker = new google.maps.Marker(carMarkerOptions)
-
-    const { strokeColor } = this.startMarker.getIcon() as google.maps.Symbol
-    this.directionsRenderer = new google.maps.DirectionsRenderer({
-      suppressMarkers: true,
-      polylineOptions: {
-        strokeColor,
-        strokeOpacity: 0.5,
-        strokeWeight: 5,
-      },
-    })
-    this.directionsRenderer.setMap(this.startMarker.getMap() as google.maps.Map)
-  }
-
-  async calculateRoute(
-    directionsResponseData?: DirectionsResponseData & { request: any },
-  ) {
-    if (directionsResponseData) {
-      const directionsResult = convertDirectionsResponseToDirectionsResult(
-        directionsResponseData,
-      )
-      this.directionsRenderer.setDirections(directionsResult)
-      return
-    }
-
-    const startPosition = this.startMarker.getPosition() as google.maps.LatLng
-    const endPosition = this.endMarker.getPosition() as google.maps.LatLng
-
-    const result = await new google.maps.DirectionsService().route({
-      origin: startPosition,
-      destination: endPosition,
-      travelMode: google.maps.TravelMode.DRIVING,
-    })
-    this.directionsRenderer.setDirections(result)
-  }
-
-  delete() {
-    this.startMarker.setMap(null)
-    this.endMarker.setMap(null)
-    this.carMarker.setMap(null)
-    this.directionsRenderer.setMap(null)
-  }
-}
 
 export const makeCarIcon = (color: string) => ({
   path: 'M23.5 7c.276 0 .5.224.5.5v.511c0 .793-.926.989-1.616.989l-1.086-2h2.202zm-1.441 3.506c.639 1.186.946 2.252.946 3.666 0 1.37-.397 2.533-1.005 3.981v1.847c0 .552-.448 1-1 1h-1.5c-.552 0-1-.448-1-1v-1h-13v1c0 .552-.448 1-1 1h-1.5c-.552 0-1-.448-1-1v-1.847c-.608-1.448-1.005-2.611-1.005-3.981 0-1.414.307-2.48.946-3.666.829-1.537 1.851-3.453 2.93-5.252.828-1.382 1.262-1.707 2.278-1.889 1.532-.275 2.918-.365 4.851-.365s3.319.09 4.851.365c1.016.182 1.45.507 2.278 1.889 1.079 1.799 2.101 3.715 2.93 5.252zm-16.059 2.994c0-.828-.672-1.5-1.5-1.5s-1.5.672-1.5 1.5.672 1.5 1.5 1.5 1.5-.672 1.5-1.5zm10 1c0-.276-.224-.5-.5-.5h-7c-.276 0-.5.224-.5.5s.224.5.5.5h7c.276 0 .5-.224.5-.5zm2.941-5.527s-.74-1.826-1.631-3.142c-.202-.298-.515-.502-.869-.566-1.511-.272-2.835-.359-4.441-.359s-2.93.087-4.441.359c-.354.063-.667.267-.869.566-.891 1.315-1.631 3.142-1.631 3.142 1.64.313 4.309.497 6.941.497s5.301-.184 6.941-.497zm2.059 4.527c0-.828-.672-1.5-1.5-1.5s-1.5.672-1.5 1.5.672 1.5 1.5 1.5 1.5-.672 1.5-1.5zm-18.298-6.5h-2.202c-.276 0-.5.224-.5.5v.511c0 .793.926.989 1.616.989l1.086-2z',
