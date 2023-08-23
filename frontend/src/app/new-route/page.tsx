97fde129a6fe9fac6cbc04fb9e21d7c1aba1e052
natanaelvich <taelima1997@gmail.com>
2023-08-22 21:23:57 -0300

    updae with new components

--- frontend/src/app/new-route/page.tsx
+++ frontend/src/app/new-route/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { Button } from '@/components/Button'
+import { Input } from '@/components/Input'
 import { useMap } from '@/hooks/useMap'
 import { useRef } from 'react'
 
@@ -9,12 +11,12 @@ export function NewRoutePage() {
 
   return (
     <div className="flex flex-row h-full">
-      <div>
-        <h1>Nova rota</h1>
-        <form className="flex flex-col">
-          <input name="source_place" placeholder="origem" />
-          <input name="destination_place" placeholder="destino" />
-          <button type="submit">Pesquisar</button>
+      <div className="flex flex-col w-1/4 py-2 px-6 gap-6">
+        <h1 className="text-2xl">Nova rota</h1>
+        <form className="flex flex-col gap-4">
+          <Input name="source_place" placeholder="origem" />
+          <Input name="destination_place" placeholder="destino" />
+          <Button type="submit">Pesquisar</Button>
         </form>
       </div>
       <div id="map" className="h-full w-full" ref={mapContainerRef}></div>
