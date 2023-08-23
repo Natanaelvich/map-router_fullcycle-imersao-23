7263b8d3974acab5e3b7e720e492ea4f320cac2f
natanaelvich <taelima1997@gmail.com>
2023-08-22 21:23:37 -0300

    add button component

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,19 @@
+import { HTMLAttributes } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+export type ButtonProps = HTMLAttributes<HTMLButtonElement>
+
+export function Button(props: ButtonProps) {
+  return (
+    <button
+      {...props}
+      className={twMerge(
+        'rounded-lg px-4 py-2 text-sm font-semibold outline-none shadow-sm',
+        'focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-violet-500',
+        'active:opacity-80',
+        'bg-yellow-500 text-white',
+        props.className,
+      )}
+    />
+  )
+}
