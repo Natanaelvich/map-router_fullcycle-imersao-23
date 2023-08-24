4ff3a6350db31f41c86ad262885d04a164b98d5f
natanaelvich <taelima1997@gmail.com>
2023-08-23 21:46:46 -0300

    fix styles input

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -9,7 +9,9 @@ export function Input(props: InputProps) {
       {...props}
       className={twMerge(
         'flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none',
-        'focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100',
+        'focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-yellow-400',
+        'bg-zinc-100 text-gray-900',
+        'placeholder-gray-500',
         props.className,
       )}
     />
