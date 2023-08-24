82680b4db5cecf0b96ca129dbc0d1a12cdca7e06
natanaelvich <taelima1997@gmail.com>
2023-08-23 21:58:02 -0300

    update connection socket with env

--- frontend/src/utils/socket-io.ts
+++ frontend/src/utils/socket-io.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client'
 
-export const socket = io('http://localhost:3000', {
+export const socket = io(process.env.NEXT_PUBLIC_NEXT_API_URL as string, {
   autoConnect: false,
 })
