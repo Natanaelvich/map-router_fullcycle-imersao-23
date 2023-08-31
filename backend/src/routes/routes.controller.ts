ca81fd6edc22aa28c16e22a24ab5835be00e6b42
natanaelvich <taelima1997@gmail.com>
2023-08-30 22:32:21 -0300

    add RouteSerializer

--- backend/src/routes/routes.controller.ts
+++ backend/src/routes/routes.controller.ts
@@ -10,24 +10,29 @@ import {
 import { RoutesService } from './routes.service';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { UpdateRouteDto } from './dto/update-route.dto';
+import { RouteSerializer } from './route.serializer';
 
 @Controller('routes')
 export class RoutesController {
   constructor(private readonly routesService: RoutesService) {}
 
   @Post()
-  create(@Body() createRouteDto: CreateRouteDto) {
-    return this.routesService.create(createRouteDto);
+  async create(@Body() createRouteDto: CreateRouteDto) {
+    //DTO - Data Transfer Object
+    const route = await this.routesService.create(createRouteDto);
+    return new RouteSerializer(route);
   }
 
   @Get()
-  findAll() {
-    return this.routesService.findAll();
+  async findAll() {
+    const routes = await this.routesService.findAll();
+    return routes.map((route) => new RouteSerializer(route));
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.routesService.findOne(id);
+  async findOne(@Param('id') id: string) {
+    const route = await this.routesService.findOne(id);
+    return new RouteSerializer(route);
   }
 
   @Patch(':id')
