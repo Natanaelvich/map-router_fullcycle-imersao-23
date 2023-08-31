81f8fb06af9955be98007e7d39baf3274c332552
natanaelvich <taelima1997@gmail.com>
2023-08-30 22:19:03 -0300

    add RoutesGateway and BullModule to route imports modules

--- backend/src/routes/routes.module.ts
+++ backend/src/routes/routes.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { RoutesController } from './routes.controller';
 import { MapsModule } from 'src/maps/maps.module';
+import { RoutesGateway } from './routes/routes.gateway';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-  imports: [MapsModule],
+  imports: [
+    MapsModule,
+    BullModule.registerQueue(
+      { name: 'new-points' },
+      { name: 'kafka-producer' },
+    ),
+  ],
   controllers: [RoutesController],
-  providers: [RoutesService],
+  providers: [RoutesService, RoutesGateway],
 })
 export class RoutesModule {}
