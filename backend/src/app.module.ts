3564b49573613e497cccdb41bde34cfbbe0cf677
natanaelvich <taelima1997@gmail.com>
2023-08-30 22:05:50 -0300

    remove inicial files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { RoutesModule } from './routes/routes.module';
 import { MapsModule } from './maps/maps.module';
@@ -13,7 +11,5 @@ import { ConfigModule } from '@nestjs/config';
     RoutesModule,
     MapsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
