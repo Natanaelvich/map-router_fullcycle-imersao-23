4929c32d203727910dd7ee11fe9f6c7c16403f6d
natanaelvich <taelima1997@gmail.com>
2023-08-31 22:40:57 -0300

    update admin page

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -3,6 +3,7 @@
 import { useCallback, useEffect, useRef } from 'react'
 import { useMap } from '@/hooks/useMap'
 import { socket } from '@/utils/socket-io'
+import { Route } from '@/utils/model'
 
 export default function AdminPage() {
   const containerMapRef = useRef<HTMLDivElement>(null)
@@ -11,62 +12,39 @@ export default function AdminPage() {
   useEffect(() => {
     socket.connect()
 
-    return () => {
-      socket.disconnect()
-    }
-  }, [])
-
-  const subscribeRoute = useCallback(
-    async (routeId: string) => {
-      console.log(routeId)
-      const response = await fetch(`http://localhost:3000/routes/${routeId}`)
-      const route = await response.json()
-      map!.removeRoute(routeId!)
-      map!.addRouteWithIcons({
-        routeId: routeId!,
-        startMarkerOptions: {
-          position: route.source.location,
-        },
-        endMarkerOptions: {
-          position: route.destination.location,
-        },
-        carMarkerOptions: {
-          position: route.source.location,
-        },
-      })
-    },
-    [map],
-  )
-
-  useEffect(() => {
-    if (!socket.connected) {
-      return
-    }
-
     socket.on(
-      'admin-new-point',
+      'admin-new-points',
       async (data: { route_id: string; lat: number; lng: number }) => {
-        console.log(data)
-        if (!map!.hasRoute(data.route_id)) {
-          await subscribeRoute(data.route_id)
+        if (!map?.hasRoute(data.route_id)) {
+          const response = await fetch(
+            `${process.env.NEXT_PUBLIC_NEXT_API_URL}/routes/${data.route_id}`,
+          )
+          const route: Route = await response.json()
+          map?.removeRoute(data.route_id)
+          await map?.addRouteWithIcons({
+            routeId: data.route_id,
+            startMarkerOptions: {
+              position: route.directions.routes[0].legs[0].start_location,
+            },
+            endMarkerOptions: {
+              position: route.directions.routes[0].legs[0].end_location,
+            },
+            carMarkerOptions: {
+              position: route.directions.routes[0].legs[0].start_location,
+            },
+          })
         }
-        map!.moveCar(data.route_id, {
+        map?.moveCar(data.route_id, {
           lat: data.lat,
           lng: data.lng,
         })
-        // if (map!.hasRoute(data.route_id)) {
-        //   map!.moveCar(data.route_id, {
-        //     lat: data.lat,
-        //     lng: data.lng,
-        //   });
-        // }
       },
     )
 
     return () => {
-      socket.off('admin-new-point')
+      socket.disconnect()
     }
-  }, [map, subscribeRoute])
+  }, [map])
 
   return (
     <div className="flex flex-row h-full">
